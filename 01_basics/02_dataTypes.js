190869cd0dec092ddfed8f0b36e74c614d9badd8
Mayank Kanojia <113011870+Mayank-019@users.noreply.github.com>
2024-07-08 14:34:31 +0000

    dataTypes and ECMA

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) // this will work in browser. But we are using nodejs
+
+console.log(3 
+    + 3 )           // code readibility should be high. Wrong way
+
+console.log("Mayank")
+
+
+let name = "Mayank"
+let age = 18
+let isLoggedIn = false
+let state;
+let temp = null;
+
+// number => 2 power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof("Mayank"))
+console.log(typeof(null))   // object --> o/p
+console.log(typeof(undefined))  // undefined --> o/p
