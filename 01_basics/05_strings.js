8f562f8efafaa2dc604586871a622bd08d126880
Mayank Kanojia <113011870+Mayank-019@users.noreply.github.com>
2024-07-09 17:52:39 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name = "Mayank"
+const repoCount = 50
+
+// older method, don't use this now
+// console.log(name + repoCount + " Value");
+
+
+// string interpolation
+console.log(`Hello, my name is ${name} and my repo count is ${repoCount}`);
